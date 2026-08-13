8ffcbabffc105c840c6eb9b72d809c8e91096c9c
NoxAlgorithm <hallowsmooth@gmail.com>
2026-08-13 16:47:18 +0200

    Update UI

--- src/features/profile/components/common/ProfileTabs.tsx
+++ src/features/profile/components/common/ProfileTabs.tsx
@@ -4,7 +4,7 @@ export type TabType = "echo" | "nexus" | "vault";
 
 const tabs: { id: TabType; label: string }[] = [
   { id: "echo", label: "جدار الصدى" },
-  { id: "nexus", label: "منشوراتي" },
+  { id: "nexus", label: "منشورات خاصة" },
   { id: "vault", label: "الخزنة" },
 ];
 
